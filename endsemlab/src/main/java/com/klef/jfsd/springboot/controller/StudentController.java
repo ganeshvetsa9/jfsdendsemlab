b46d5541d6aecf51b630d19686d44703273dc5b0
ganeshvetsa9 <149612410+ganeshvetsa9@users.noreply.github.com>
2024-12-07 12:49:56 +0530

    Add files via upload

---
+++ endsemlab/src/main/java/com/klef/jfsd/springboot/controller/StudentController.java
@@ -0,0 +1,70 @@
+package com.klef.jfsd.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.klef.jfsd.springboot.model.Student;
+import com.klef.jfsd.springboot.service.StudentService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("student")
+public class StudentController 
+{
+	@Autowired
+	private StudentService servive;
+	
+	@GetMapping("/")
+	public String home(){
+		return "Rest API with JPA";
+	}
+	
+	@PostMapping("add")
+	public String addstudent(@RequestBody   Student student) {
+		return servive.addstudent(student);
+		
+	}
+	
+	@GetMapping("viewall")
+	public List<Student> viewallStudent()
+	{
+		return servive.displayallStudents();
+	}
+	
+	@GetMapping("display")
+	public Student displaystudentbyid(@RequestParam("id")  int sid)
+	{
+		return servive.displayStudentByID(sid);
+	}
+	
+	@DeleteMapping("delete")
+	public String deletestudent(@RequestParam("id")  int sid)
+	{
+		return servive.deletestudent(sid);
+	}
+	
+
+	@PutMapping("update")
+	public String updatestudent(@RequestBody  Student s)
+	{
+		return servive.updatestudent(s);
+	}
+
+
+	
+	
+	
+	
+
+}
---
+++ endsemlab/src/main/java/com/klef/jfsd/springboot/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.klef.jfsd.springboot.service;
+
+import java.util.List;
+
+import com.klef.jfsd.springboot.model.Student;
+
+public interface StudentService
+{
+	public String addstudent(Student s);
+	public String updatestudent(Student s);
+	public String deletestudent(int sid);
+	public Student displayStudentByID(int sid);
+	public List<Student> displayallStudents();
+	
+
+}
